b6e75f223af27e4b23451888b2bfe67371100756
죠 <stop.ho@khu.ac.kr>
2025-01-14 22:45:14 +0900

    feat: 메인 페이지에 불필요한 부분 제거 (미완성)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,14 +17,6 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
       </div>
     </header>
   );
@@ -34,12 +26,13 @@ export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        {/* <HomepageFeatures /> */}
+        작성중!
       </main>
     </Layout>
   );
